decb5fde932453ec0dfca394a802c2f6afa533e9
Lewis Wilcock <l.d.wilcock@gmail.com>
2013-03-21 17:38:18 +0000

    Made initial menu Button pack added

--- AngryMasons/AngryMasons/src/com/lewis/angrymasons/Screens/MainMenu.java
+++ AngryMasons/AngryMasons/src/com/lewis/angrymasons/Screens/MainMenu.java
@@ -3,34 +3,105 @@ package com.lewis.angrymasons.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.lewis.angrymasons.AngryMasons;
 
 public class MainMenu implements Screen{
 	
+	AngryMasons game;
+	
+	Stage stage;
+	BitmapFont black;
+	BitmapFont white;
+	TextureAtlas atlas;
+	Skin skin;
+	SpriteBatch batch;
+	TextButton button;
+	
 	public MainMenu(AngryMasons game)
 	{
-		
+		this.game = game;
 	}
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		stage.act(delta);
+		
+		batch.begin();
+		
+		stage.draw();
+		white.draw(batch,  "Angry Masons",  Gdx.graphics.getWidth() / 2 - 15, Gdx.graphics.getHeight() / 2 + 100);
+		
+		batch.end();
 	}
 
 	@Override
 	public void resize(int width, int height) {
+		if(stage == null)
+		{
+			stage = new Stage(width, height, true);
+		}
+		stage.clear();
+		
+		Gdx.input.setInputProcessor(stage);
+		
+		TextButtonStyle style = new TextButtonStyle();
+		style.up = skin.getDrawable("buttonnormal");
+		style.down = skin.getDrawable("buttonpressed");
+		style.font = black;
+		
+		button = new TextButton("PLAY", style);
+		button.setWidth(400);
+		button.setHeight(100);
+		button.setX(Gdx.graphics.getWidth() / 2 - button.getWidth() / 2);
+		button.setY(Gdx.graphics.getHeight() / 2 - button.getHeight() / 2);
+		
+		button.addListener(new InputListener(){
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
+			{
+				return true;
+			}
+			
+			// Must handle both touch up and touch down otherwise it doesn't work
+			
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button)
+			{
+				System.out.println("Up");
+				game.setScreen(new Game(game));
 
+			}
+		});
+		
+		stage.addActor(button);
+		
 	}
 
 	@Override
 	public void show() {
-
+		batch = new SpriteBatch();
+		atlas = new TextureAtlas("data/button.pack");
+		
+		skin = new Skin();
+		skin.addRegions(atlas);
+		
+		white = new BitmapFont(Gdx.files.internal("data/whitefont.fnt"), false);
+		black = new BitmapFont(Gdx.files.internal("data/font.fnt"), false);
 	}
 
 	@Override
 	public void hide() {
-
+		dispose();
 	}
 
 	@Override
@@ -45,7 +116,12 @@ public class MainMenu implements Screen{
 
 	@Override
 	public void dispose() {
-
+		batch.dispose();
+		skin.dispose();
+		atlas.dispose();
+		black.dispose();
+		white.dispose();
+		stage.dispose();
 	}
 
 }
